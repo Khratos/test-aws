360971a090295d9326759ba1b0e035f46d7f8216
Khratos <vmeis.cs@gmail.com>
2022-07-15 17:14:43 +0200

    35

--- PythonAWS/book/31.py
+++ PythonAWS/book/31.py
@@ -7,10 +7,8 @@ def create_bucket(bucket_name):
     if bucket:
         print ('Bucket (%s) already exists' % bucket_name)
     else:
-    # Let's try to create the bucket. This will fail if
-    # the bucket has already been created by someone else.
-    try:
-        bucket = s3.create_bucket(bucket_name)
-    except s3.provider.storage_create_error, e:
-        print ('Bucket (%s) is owned by another user' % bucket_name)
+        try:
+            bucket = s3.create_bucket(bucket_name)
+        except s3.provider.storage_create_error, e:
+            print ('Bucket (%s) is owned by another user' % bucket_name)
     return bucket
---
+++ PythonAWS/book/35.py
@@ -0,0 +1,12 @@
+import boto
+def bucket_du(bucket_name):
+
+    s3 = boto.connect_s3()
+    total_bytes = 0
+    bucket = s3.lookup(bucket_name)
+    if bucket:
+        for key in bucket:
+            total_bytes += key.size
+    else:
+        print('Warning: bucket %s was not found!' % bucket_name)
+    return total_bytes
