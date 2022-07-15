7f68e2bd7922658bc59ee1f2619642ad744e8713
Khratos <vmeis.cs@gmail.com>
2022-07-15 21:47:28 +0200

    changes

--- PythonAWS/book/24.py
+++ PythonAWS/book/24.py
@@ -1,6 +1,9 @@
+import base64
 import boto
+
 ec2 = boto.connect_ec2()
 fp = open('key.pub')
 material = fp.read()
 fp.close()
-key_pair = ec2.import_key_pair('key', material)
+
+key_pair = ec2.import_key_pair(base64.b64encode('key'), material)
